c970df99d677577764a881403d2103aa462ae610
lpeil <luan@peil.dev>
2021-03-26 15:59:33 -0300

    news from many categories

--- services/news.js
+++ services/news.js
@@ -1,26 +1,53 @@
 const axios = require('axios');
 require('dotenv').config();
 
-const url = "https://newsapi.org/v2/top-headlines?country=br&category=entertainment&pageSize=5&apiKey=";
-const apiKey = process.env.NEWS_API_KEY;
 const newsChatId = "824358432760791060";
 
 const startNews = async (content) => {
   const channel = await content.bot.channels.fetch(newsChatId);
+  const categories = getCategories()   
 
-   axios.get(url + apiKey)
+  for(const category of categories) {
+    getNewsFromCategory(category, channel);
+  }
+}
+
+const getNewsFromCategory = async (category, channel) => {
+  const apiKey = process.env.NEWS_API_KEY;
+  const url = `https://newsapi.org/v2/top-headlines?country=br&category=${category.id}&apiKey=${apiKey}`;
+
+  return await axios.get(url)
     .then((response) => response.data)
     .then((data) => {
-      for(const article of data.articles) {
-        if(article.title && article.urlToImage) {
-          channel.send(
-            article.title,
-            {files: [article.urlToImage]}
-          )
-          break;
-        }
-      }
+      checkArticles(category, data.articles, channel);
     })
+}
+
+const checkArticles = async (category, articles, channel) => {
+  for(const article of articles) {
+    if(article.title && article.urlToImage) {
+      return await sendArticleIntoChannel(category, article, channel)
+    }
+  }
+}
+
+const sendArticleIntoChannel = async (category, article, channel) => {
+  return await channel.send(
+    `[${category.name}] ${article.title} \n ${article.url}`,
+    {files: [article.urlToImage]}
+  )
+}
+
+const getCategories = () => {
+  return [
+    { id: 'entertainment', name: 'Entretenimento'},
+    { id: 'business', name: 'Negócios'},
+    { id: 'general', name: 'Geral'},
+    { id: 'health', name: 'Saúde'},
+    { id: 'science', name: 'Ciência'},
+    { id: 'sports', name: 'Esportes'},
+    { id: 'technology', name: 'Têcnologia'},
+  ]
 }
 
 module.exports = startNews;
